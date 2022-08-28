f9ee3d53ff96df48a5c4b6b1b803f7fe4fc7f9d9
makelarisjr <8687447+makelarisjr@users.noreply.github.com>
2022-08-28 16:50:28 +0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
-bash -c "/tmp/vendor/bin/pint $1"
+PINT_OUTPUT=$(/tmp/vendor/bin/pint $1)
+
+if [[ "$2" == "true" ]]; then
+    echo -e "::set-output name=pint_output::$PINT_OUTPUT"
+else
+    echo -e "$PINT_OUTPUT"
+fi
